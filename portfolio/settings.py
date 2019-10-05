2a48ca2c895e52d685ec900888d99b1a50ed53f7
DenisCalixto <deniscalixto@gmail.com>
2019-10-04 19:43:29 -0700

    JWT and Django Rest Framework with Session Authorization (for created users)

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,6 +138,25 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         "rest_framework.authentication.SessionAuthentication",
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated', )
+# }
+
 try:
     from .local_settings import *
 except ImportError:
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    url('api-auth/', include('rest_framework.urls')),
+    url('api/token/', TokenObtainPairView.as_view()),
+    url('api/token/refresh/', TokenRefreshView.as_view()),
     path('blog/', include('blog.urls')),
     path('api/', include('jobs.urls')),
     path("api/", include("users.urls"), name="users"),
